6c5faacd30ed41a46bc182c528d3955104dca4cb
Chris Scribner <scriby@gmail.com>
2017-01-30 11:16:59 -0800

    Fix an issue with imported CSS modules

--- test/webpack.config.babel.js
+++ test/webpack.config.babel.js
@@ -7,11 +7,12 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.ts$/, loaders: ['babel', 'ts'] },
-      { test: /example.css$/, loader: '../src/index.js?modules' },
-      { test: /example-camelcase.css$/, loader: '../src/index.js?modules&camelCase' },
-      { test: /example-namedexport.css$/, loader: '../src/index.js?modules&namedExport' },
-      { test: /example-camelcase-namedexport.css$/, loader: '../src/index.js?modules&camelCase&namedExport' },
-      { test: /example-no-css-modules.css$/, loader: '../src/index.js' },
+      { test: /example\.css$/, loader: '../src/index.js?modules' },
+      { test: /example-camelcase\.css$/, loader: '../src/index.js?modules&camelCase' },
+      { test: /example-namedexport\.css$/, loader: '../src/index.js?modules&namedExport' },
+      { test: /example-camelcase-namedexport\.css$/, loader: '../src/index.js?modules&camelCase&namedExport' },
+      { test: /example-no-css-modules\.css$/, loader: '../src/index.js' },
+      { test: /example-compose\.css$/, loader: '../src/index.js?modules&camelCase&namedExport' }
     ]
   }
 };
