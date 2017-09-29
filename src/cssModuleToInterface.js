6d1e23a6eddd02748628d2675279da833230ffc4
John Papandriopoulos <jpap@users.noreply.github.com>
2017-09-28 19:19:26 -0700

    Support for placing .d.ts files in a different directory

--- src/cssModuleToInterface.js
+++ src/cssModuleToInterface.js
@@ -28,10 +28,52 @@ export const filterNonWordClasses = (cssModuleKeys) => {
   return [filteredClassNames, nonWordClassNames,];
 };
 
-export const filenameToTypingsFilename = (filename) => {
-  const dirName = path.dirname(filename);
-  const baseName = path.basename(filename);
-  return path.join(dirName, `${baseName}.d.ts`);
+export const filenameToTypingsFilename = (filename, pathPrefix, contextPath) => {
+  // Default implementation: co-locate .d.ts with the resource
+  const pathPrefixCallbackDefault = function (filename) {
+    const dirName = path.dirname(filename);
+    const baseName = path.basename(filename);
+    return path.join(dirName, baseName + '.d.ts');
+  };
+
+  let pathPrefixCallback = pathPrefixCallbackDefault;
+
+  if (pathPrefix) {
+    switch (typeof pathPrefix) {
+    case 'string': {
+      if (pathPrefix.endsWith(path.sep)) {
+        pathPrefix = pathPrefix.slice(0, -1);
+      }
+      let dirName = path.dirname(filename);
+      const baseName = path.basename(filename);
+      // There are three cases here:
+      //  1. filename is within the webpack context: add the prefix
+      if (dirName.startsWith(contextPath)) {
+        dirName = dirName.slice(contextPath.length);
+        pathPrefixCallback = function () {
+          return path.join(contextPath, pathPrefix, dirName, baseName + '.d.ts');
+        };
+      }
+      //  2. pathPrefix is absolute
+      //  3. filename is outside the webpack context
+      else {
+        // Use the default implementation for co-locating the .d.ts
+      }
+      break;
+    }
+
+    case 'function':
+      // Use the user-configured filename mapping function
+      pathPrefixCallback = pathPrefix;
+      break;
+
+    default:
+      // Bad configuration
+      return undefined;
+    }
+  }
+
+  return pathPrefixCallback(filename, contextPath);
 };
 
 export const generateNamedExports = (cssModuleKeys) => {
